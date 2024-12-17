ad0da3ec6b210a69384d1449617dea5c95e8048b
mitchelldesjardins <mitchdes51@gmail.com>
2024-12-16 21:24:07 -0500

    Initial Commit

---
+++ RPSGame/RPSGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSGameApp.swift
+//  RPSGame
+//
+//  Created by Mitchell Desjardins on 2024-12-16.
+//
+
+import SwiftUI
+
+@main
+struct RPSGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RPSGameTests/RPSGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  RPSGameTests.swift
+//  RPSGameTests
+//
+//  Created by Mitchell Desjardins on 2024-12-16.
+//
+
+import Testing
+@testable import RPSGame
+
+struct RPSGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
